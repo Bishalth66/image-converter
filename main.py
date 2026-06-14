a62bd4eada74286b8390d4efc1063b3724f193a5
Bishal Thapa <bishalth66@gmail.com>
2026-06-14 16:32:07 +0545

    feat: Added Image to Webp Converter logic

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from image-converter!")
+from PIL import Image
+
+def convert_to_webp(filename, path="images/"):
+   extension = filename.split('.')[-1]
+   fname = filename.split('.')[0]
+   img = Image.open(path + filename)
+   
+   if extension == "png":
+      img.save((path+fname+".webp"),"webp", lossless=True)
+   else:
+      img.save((path+fname+".webp"),"webp", quality=85)
 
 
 if __name__ == "__main__":
-    main()
+   convert_to_webp("image.jpg")
